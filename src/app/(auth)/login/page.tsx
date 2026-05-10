3fca3364a62e6e45c0a685c98cfe12da3ca4c45d
quoclam204 <nguyenlequoclam@gmail.com>
2026-05-10 10:38:56 +0700

    fix: lỗi đăng nhập

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${getURL()}auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
     if (error) {
